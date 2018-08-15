555cc6dd7749f0e21b4678497ca1cac142a8d5c4
Ben Web <ben@benweb.com>
2018-08-15 14:14:57 +0100

    added seed comments its and factory method

--- seed/seed.js
+++ seed/seed.js
@@ -3,41 +3,56 @@
 const
   mongoose = require('mongoose'),
   { Article, Comment, Topic, User } = require('../models'),
-  { formatArticleData } = require('./utils');
+  { formatArticleData, formatCommentData } = require('./utils');
 
-const seedDB = ({ topicData, userData, articleData }) => {
+const seedDB = ({ topicData, userData, articleData, commentData }) => {
   return mongoose.connection.dropDatabase()
-
+    // Insert Topics and Users
     .then(() => {
       return Promise.all([
         Topic.insertMany(topicData),
         User.insertMany(userData)
       ]);
     })
-
+    // Insert Articles
     .then(([
       topicDocs,
       userDocs
     ]) => {
-
       return Promise.all([
         Article.insertMany(formatArticleData(articleData, userDocs)),
         topicDocs,
         userDocs
       ]);
     })
-
+    // Insert Comments
     .then(([
       articleDocs,
       topicDocs,
       userDocs
     ]) => {
+      return Promise.all([
+        Comment.insertMany(formatCommentData(commentData, articleDocs, userDocs)),
+        articleDocs,
+        topicDocs,
+        userDocs
+      ]);
+    })
+    // Log outcomes
+    .then(([
+      commentDocs,
+      articleDocs,
+      topicDocs,
+      userDocs
+    ]) => {
       console.log(topicDocs.length, ' topics added to DB');
-      console.log('First Topic >>>', topicDocs[0]);
+      console.log('<<< First Topic >>>\n', topicDocs[0]);
       console.log(userDocs.length, ' users added to DB');
-      console.log('First User >>>', userDocs[0]);
+      console.log('<<< First User >>>\n', userDocs[0]);
       console.log(articleDocs.length, ' articles added to DB');
-      console.log('First Article >>>', articleDocs[0]);
+      console.log('<<< First Article >>>\n', articleDocs[0]);
+      console.log(commentDocs.length, ' comments added to DB');
+      console.log('<<< First Comment >>>\n', commentDocs[0]);
     });
 };
 
--- seed/utils/index.js
+++ seed/utils/index.js
@@ -1,12 +1,10 @@
 'use strict';
-const
-  _ = require('lodash');
 
 exports.formatArticleData = (articleData, userDocs) => {
   return articleData.map(article => {
     const
       belongs_to = article.topic,
-      created_by = _.find(userDocs, (user) => user.username === article.created_by)._id;
+      created_by = userDocs.find(user => user.username === article.created_by)._id;
     return {
       ...article,
       belongs_to,
@@ -14,3 +12,17 @@ exports.formatArticleData = (articleData, userDocs) => {
     };
   });
 };
+
+exports.formatCommentData = (commentData, articleDocs, userDocs) => {
+  return commentData.map(comment => {
+    const
+      belongs_to = articleDocs.find(article => article.title === comment.belongs_to)._id,
+      created_by = userDocs.find(user => user.username === comment.created_by)._id;
+    return {
+      ...comment,
+      belongs_to,
+      created_by
+    };
+  });
+};
+
